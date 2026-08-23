6100c33dad8741bc24896cf6f4334d929e813175
kalviisk <kalninskal@gmail.com>
2026-08-24 02:16:40 +0300

    generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function handler(req, res) { // v2
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
